5ad1d192b0009a5aa3ed5ac7fd12277423addefd
Siarhei Lunski <the.sergaunt@gmail.com>
2019-02-19 21:13:00 +0300

    install material-ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,50 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Header = () => {
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
+
+const Header = props => {
+  const { classes } = props;
   return (
-    <header>
-      <NavLink to="/">Main Page</NavLink>
-      <NavLink to="/profile">Profile</NavLink>
+    <header className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="Menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            <NavLink to="/">Main Page</NavLink>
+          </Typography>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            <NavLink to="/profile">Profile</NavLink>
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
     </header>
   );
 };
 
-export default Header;
+export default withStyles(styles)(Header);
