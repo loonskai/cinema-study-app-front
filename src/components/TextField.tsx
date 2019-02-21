1d6d15555bdedad763ccbae1266748a6707a2223
Siarhei Lunski <the.sergaunt@gmail.com>
2019-02-21 12:14:48 +0300

    style input icons

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -3,30 +3,39 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import LocalMovies from '@material-ui/icons/LocalMovies';
 import LocationCity from '@material-ui/icons/LocationCity';
-import { getConfirmation } from 'history/DOMUtils';
+import styled from 'styled-components';
 
 interface Props {
   label: string;
   icon?: string;
 }
 
-const TextFieldStyled = ({ label, icon }: Props) => {
-  const getInputIcon = () => {
-    if (!icon) return null;
-    switch (icon) {
-      case 'movie':
-        return <LocalMovies fontSize="large" />;
-      case 'city':
-        return <LocationCity fontSize="large" />;
-      default:
-        return null;
-    }
-  };
+const IconStyled = styled(({ icon, ...other }) => {
+  switch (icon) {
+    case 'movie':
+      return (
+        <LocalMovies fontSize="large" classes={{ root: 'root' }} {...other} />
+      );
+    case 'city':
+      return (
+        <LocationCity fontSize="large" classes={{ root: 'root' }} {...other} />
+      );
+    default:
+      return null;
+  }
+})`
+  &.root {
+    color: #009688;
+  }
+`;
 
+const TextFieldStyled = ({ label, icon }: Props) => {
   return (
-    <Grid container spacing={8} justify="center" alignItems="center">
-      <Grid item>{getInputIcon()}</Grid>
-      <Grid item lg={7}>
+    <Grid container={true} spacing={8} justify="center" alignItems="center">
+      <Grid item={true}>
+        <IconStyled icon={icon} />
+      </Grid>
+      <Grid item={true} lg={7}>
         <TextField
           id="outlined-search"
           label={label}
