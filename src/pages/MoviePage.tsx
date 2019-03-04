e70203038f61e3d179ca25b74021c0598aa947db
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-04 15:42:19 +0300

    add basic markup to MoviePage

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -1,8 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 
 import api from './../ApiService';
 import Loader from './../components/Loader';
+import PageTitle from './../components/PageTitle';
+
+const MovieInfoContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  @media screen and (min-width: 768px) {
+    justify-content: flex-start;
+    flex-wrap: nowrap;
+  }
+`;
+
+const StyledPoster = styled.img`
+  width: 100%;
+  max-width: 350px;
+  height: auto;
+  margin: 1rem 0;
+  align-self: center;
+`;
+
+const StyledDescription = styled.div`
+  @media screen and (min-width: 768px) {
+    padding: 1.5rem;
+  }
+`;
 
 const MoviePage = ({ movie, match }: any) => {
   const [loadedMovie, setMovie] = useState(movie);
@@ -26,12 +52,14 @@ const MoviePage = ({ movie, match }: any) => {
     <Loader />
   ) : (
     <div>
-      <h1>{loadedMovie.original_title}</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${loadedMovie.poster_path}`}
-        alt={loadedMovie.original_title}
-      />
-      <p>{loadedMovie.overview}</p>
+      <PageTitle text={loadedMovie.original_title} />
+      <MovieInfoContainer>
+        <StyledPoster
+          src={`https://image.tmdb.org/t/p/w500${loadedMovie.poster_path}`}
+          alt={loadedMovie.original_title}
+        />
+        <StyledDescription>{loadedMovie.overview}</StyledDescription>
+      </MovieInfoContainer>
     </div>
   );
 };
