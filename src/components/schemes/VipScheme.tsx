899eed0d5d58cfbe8ce4042f431276a1b69c726a
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-07 08:57:21 +0300

    add RowTitle

--- src/components/schemes/VipScheme.tsx
+++ src/components/schemes/VipScheme.tsx
@@ -12,10 +12,21 @@ const RowItem = styled.div`
   justify-content: center;
 `;
 
+const RowTitle = styled.span`
+  height: 30px;
+  margin-left: -50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 1rem;
+  font-size: 12px;
+`;
+
 const VipScheme = ({ items }: any) => {
   const renderItems = () => {
     return items.map((item: { row: number; seats: any }, index: number) => (
       <RowItem key={index}>
+        <RowTitle>row {item.row}</RowTitle>
         {item.seats.map((seat: any, index: number) => (
           <SeatItem
             key={index}
