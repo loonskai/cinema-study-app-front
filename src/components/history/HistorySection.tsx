ba5af25a7cf39c987d793c409b53674685280ee5
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-09 23:31:27 +0300

    add basic order item styles

--- src/components/history/HistorySection.tsx
+++ src/components/history/HistorySection.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import HistorySectionTabs from './HistorySectionTabs';
-import { greyColor } from '../../constants';
+import { greyColor, whiteColor, containerGreyColor } from '../../constants';
 
 const Container = styled.div`
   width: 100%;
@@ -15,7 +15,36 @@ const Container = styled.div`
 `;
 
 const HistoryContent = styled.div`
+  width: 100%;
   padding: 2rem;
+`;
+
+const OrderContainer = styled.div<any>`
+  width: 100%;
+  margin-bottom: 1rem;
+  padding: 1rem;
+  background-color: ${({ isRelevant }: { isRelevant: boolean }) =>
+    isRelevant ? whiteColor : containerGreyColor};
+  border-radius: 10px;
+  box-shadow: ${({ isRelevant }: { isRelevant: boolean }) =>
+    isRelevant ? '0px 0px 20px -5px rgba(0, 0, 0, 0.3)' : 'none'};
+`;
+
+const MainTextRow = styled.div`
+  font-weight: 700;
+  margin-bottom: 0.3rem;
+
+  &:last-child {
+    margin-bottom: 1rem;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.175rem;
+  }
+`;
+
+const TextRow = styled.div`
+  margin-bottom: 0.3rem;
 `;
 
 const HistorySection = ({ orders }: any) => {
@@ -38,17 +67,22 @@ const HistorySection = ({ orders }: any) => {
   const renderOrders = () => {
     const ordersToRender = orders[tabSelected];
     if (!ordersToRender) return 'Nothing found';
-    return ordersToRender.map((order: any) => {
+    return ordersToRender.map((order: any, index: number) => {
       return (
-        <div>
-          <div>{order.city}</div>
-          <div>{order.cinema}</div>
-          <div>{order.date}</div>
-          <div>{order.time}</div>
-          <div>Link: {order.movie.title}</div>
-          <div>Tickets amount: {order.order.length}</div>
-          <div>Total price: {order.totalPrice}</div>
-        </div>
+        <OrderContainer
+          key={index.toString()}
+          isRelevant={tabSelected === 'upcoming'}
+        >
+          <MainTextRow>
+            {order.city}, {order.cinema} cinema
+          </MainTextRow>
+          <MainTextRow>
+            {order.date}, {order.time}
+          </MainTextRow>
+          <TextRow>Movie: {order.movie.title}</TextRow>
+          <TextRow>Tickets amount: {order.order.length}</TextRow>
+          <TextRow>Total price: ${order.totalPrice}</TextRow>
+        </OrderContainer>
       );
     });
   };
