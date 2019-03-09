09eece613c62e3742c13481a7b0adc5918198bf9
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-09 16:02:13 +0300

    fix BonusSelectItem styles

--- src/components/bonus/BonusSelectItem.tsx
+++ src/components/bonus/BonusSelectItem.tsx
@@ -4,23 +4,34 @@ import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 
 import RoundButton from '../buttons/RoundButton';
-import { addButtonColor, removeButtonColor } from '../../constants';
+import { addButtonColor, removeButtonColor, greyColor } from '../../constants';
 
 const Container = styled.div`
-  margin-bottom: 1rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  padding: 0.7rem 0;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid ${greyColor};
+  }
 `;
 
-const BonusController = styled.div``;
+const BonusController = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ValueContainer = styled.div``;
 
 const BonusSelectItem = ({ bonus, handleBonusesUpdate, value }: any) => {
   const bonusKey = Object.keys(bonus)[0];
   const bonusTitle =
     Object.keys(bonus)[0]
       .charAt(0)
-      .toUpperCase() + bonusKey.slice(1);
+      .toUpperCase() +
+    bonusKey.slice(1) +
+    ` ($${bonus[bonusKey].price})`;
 
   const handleBonusClick = (type: string) => {
     handleBonusesUpdate(type, bonus);
@@ -30,7 +41,7 @@ const BonusSelectItem = ({ bonus, handleBonusesUpdate, value }: any) => {
     <Container>
       {bonusTitle}
       <BonusController>
-        <div>{value}</div>
+        <ValueContainer>{value}</ValueContainer>
         <RoundButton
           bgColor={addButtonColor}
           icon={<AddIcon />}
