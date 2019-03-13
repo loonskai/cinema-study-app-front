6ee1690ec76cfe156ae8e7c3ee508b136625d5e8
Siarhei Lunski <the.sergaunt@gmail.com>
2019-03-13 14:35:58 +0300

    add timer

--- src/components/seats/OrderTimer.tsx
+++ src/components/seats/OrderTimer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -18,13 +18,72 @@ const Time = styled.div`
   font-weight: 700;
 `;
 
-const OrderTimer = () => {
-  return (
-    <Container>
-      <span>Reservation time:</span>
-      <Time>15:00</Time>
-    </Container>
-  );
-};
+class OrderTimer extends React.Component {
+  state: {
+    time: {
+      minutes?: string;
+      seconds?: string;
+    };
+    seconds: number;
+  };
+  timer: any;
+
+  constructor(props: any) {
+    super(props);
+    this.state = { time: {}, seconds: 10 };
+    this.timer = 0;
+    this.countDown = this.countDown.bind(this);
+  }
+
+  secondsToTime(secs: number) {
+    let minutes: string | number = parseInt(`${secs / 60}`, 10);
+    let seconds: string | number = parseInt(`${secs % 60}`, 10);
+
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    const obj = { minutes, seconds };
+    return obj;
+  }
+
+  componentDidMount() {
+    if (this.timer === 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+    const timeLeftVar = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeftVar });
+  }
+
+  startTimer() {
+    if (this.timer === 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
+    }
+  }
+
+  countDown() {
+    this.setState((prevState: any) => {
+      const updatedSeconds = prevState.seconds - 1;
+      if (updatedSeconds === 0) {
+        clearInterval(this.timer);
+      }
+      return {
+        time: this.secondsToTime(updatedSeconds),
+        seconds: updatedSeconds
+      };
+    });
+  }
+
+  render() {
+    const { time } = this.state;
+    return (
+      <Container>
+        <span>Reservation time:</span>
+        <Time>
+          {time.minutes}:{time.seconds}
+        </Time>
+      </Container>
+    );
+  }
+}
 
 export default OrderTimer;
